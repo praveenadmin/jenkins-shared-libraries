3ec796f3bcdb33f39ff9392fcdeb6c9fba1a22d6
praveenadmin <45735735+praveenadmin@users.noreply.github.com>
2024-12-03 18:24:40 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,4 +1,3 @@
-def call(String repoUrl, String branch) {
-    git url: repoUrl, branch: branch
-    echo "Checked out ${branch} from ${repoUrl}"
+def call(String appName, String dockerHubUser, String tag) {
+    sh "docker build -t ${dockerHubUser}/${appName}:${tag} ."
 }
