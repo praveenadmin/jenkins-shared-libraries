6669c2ce0213d115bffc1d6f99f12367e347b609
praveenadmin <45735735+praveenadmin@users.noreply.github.com>
2024-12-03 18:08:22 +0530

    Update gitcheck.groovy

--- vars/gitcheck.groovy
+++ vars/gitcheck.groovy
@@ -1,5 +1,3 @@
 def call(string url, string branch){
-  echo "This is cloning stage"
-  git url: "${url}", branch: "${branch}"
-  echo "Code is cloned"
-}
+  git url:"${url}", branch:"${branch}"
+  }
